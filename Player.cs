0bec2660e4196c24ac2e5185410c8fe4f2590038
Teyras <teyras@gmail.com>
2013-07-15 21:30:58 +0200

    added HitTime property to Player

--- Player.cs
+++ Player.cs
@@ -8,6 +8,7 @@ namespace Bounce
 		public Direction SteeringDirection;
 		public Queue<Field> Trail = new Queue<Field> ();
 		public Field BaseField;
+		public DateTime HitTime = DateTime.Now;
 
 		public Player (int x, int y): base (x, y)
 		{
